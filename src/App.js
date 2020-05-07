b7751e4065c0934ce0327a8926e7769e8f2aac33
Greg Brewton <gbrewton1980@gmail.com>
2020-05-07 15:18:09 -0600

    Adds, styles mobile menu - removes Header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React, {useState} from "react";
+import React, {useEffect} from "react";
+import M from 'materialize-css'
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
 import "./App.css";
+import "materialize-css/dist/css/materialize.min.css"
 import Home from "./components/home/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
 
-function App(props) {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+function App() {
+  useEffect(() => {
+    M.AutoInit()
+  })
   return (
     <div className="App">
       <Router>
         <div>
-          <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+          <Navbar/>
           <Switch>
             <Route path="/" exact><Home/></Route>
             <Route path="/Projects"><Projects/></Route>
