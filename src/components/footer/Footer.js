99eacb3fd70d17917f8c8873a99a0bf559757efa
Greg Brewton <gbrewton1980@gmail.com>
2020-05-07 18:51:45 -0600

    Adds parallax scrolling, footer, media queries

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( <div className="row black white-text">
+        <div className="col s12">
+            <p className="center-align">Footer</p>
+        </div>
+    </div> );
+}
+ 
+export default Footer;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
+import M from "materialize-css";
 import "./home.scss";
 import responsive from "../../images/responsive.svg";
+import keyboard from "../../images/keyboard.jpg";
+import code from "../../images/code.jpg";
 
 const Home = () => {
+  useEffect(() => {
+    const elems = document.querySelectorAll(".parallax");
+    M.Parallax.init(elems, { responsiveThreshold: 500 });
+  });
   return (
     <div id="home">
-      <h1 className="center-align">Progressive Web Application Development</h1>
-      <p className="center-align">
-        I design and build websites optimized for today's mobile user.
-      </p>
-      <div className="strip with-opacity">
+      <div className="parallax-container">
+        <div className="parallax">
+          <img src={keyboard} alt="laptop keyboard closeup" />
+        </div>
+      </div>
+      <div className="section white">
         <div className="home-grid-container">
-          <div className="home-text hide-on-med-and-down"></div>
-          <div className="home-image">
+          <div className="home-text">
+            <div className="container">
+              <h2>Progressive Web Application development for today's users</h2>
+              <br></br>
+              <p>
+                At Studio42, I design websites for every display size and create
+                applications that can be downloaded to your Android or Apple
+                device just like a native app to keep your users engaged.
+              </p>
+            </div>
+          </div>
+          <div className="home-image valign-wrapper">
             <img
-              style={{maxWidth:"100%"}}
+              style={{ maxWidth: "100%" }}
               src={responsive}
               alt="multiple size displays"
             />
           </div>
+        </div>
+      </div>
+      <div className="parallax-container">
+        <div className="parallax">
+          <img src={code} alt="screen with lines of code" />
         </div>
       </div>
     </div>
