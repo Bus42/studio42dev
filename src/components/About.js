f03f5c7417a7b02629e0a70f29897473425678d5
Greg Brewton <gbrewton1980@gmail.com>
2020-05-12 22:31:58 -0600

    Adds crap to About

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,34 @@ const About = () => {
   return (
     <main>
       <div>
-        <h1 className="center-align">About Me</h1>
-        <div style={{display: "flex", flexFlow: "column", justifyContent: "center", alignItems: "center"}} >
+        <h1 className="center-align">Hi, I'm Greg</h1>
+        <div className="container">
+          <p>
+            I started learning JavaScript in 2016, without any frameworks or
+            libraries, so I know exactly what is happening "under the hood" when
+            I write code with a framework or library. Since then I have focused
+            my learning into using React to build single-page applications in
+            JavaScript and using Firebase to deploy my apps and handle hosting,
+            authentication, cloud functions, analytics, realtime database
+            functions, and more.
+          </p>
+          <p>
+            Many of the sites built with site builders are slow, heavy, and
+            loaded with errors that slow loading time and lose customers. I
+            wanted to provide that same quality of aesthetics in custom-built
+            applications designed for better accessibility and speed. Faster
+            load times mean higher retention and more conversions of visitors to
+            customers.
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexFlow: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <div
             class="fb-page"
             data-tabs="timeline"
