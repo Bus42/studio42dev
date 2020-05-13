356a4310d5ddc4e55af143c27fe02504919710ad
Greg Brewton <gbrewton1980@gmail.com>
2020-05-12 22:14:50 -0600

    adds facebook widget to About

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return ( <main>
-        <div>
-            <h1 className="center-align">About component</h1>
-            <p></p>
+  return (
+    <main>
+      <div>
+        <h1 className="center-align">About Me</h1>
+        <div style={{display: "flex", flexFlow: "column", justifyContent: "center", alignItems: "center"}} >
+          <div
+            class="fb-page"
+            data-tabs="timeline"
+            data-href="https://www.facebook.com/pg/studio42dev/"
+            data-width="380"
+          ></div>
         </div>
-    </main> );
-}
- 
+      </div>
+    </main>
+  );
+};
+
 export default About;
