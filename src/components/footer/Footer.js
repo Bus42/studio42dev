fab707125c387be389056102a33105103b987020
Greg Brewton <gbrewton1980@gmail.com>
2020-05-15 19:54:32 -0600

    Adds studio42dev.com email link to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,12 +10,14 @@ const Footer = () => {
             <h5 className="white-text">Studio42 Web Development</h5>
 
             <p className="white-text text-darken-2">
-              <button
-              className="btn waves-effect waves-light grey darken-3 white-text text-darken-2"
-              style={{margin: "0 1em 1em 0"}}
-              >
-                <i className="material-icons">mail</i>
-              </button>
+              <a href="mailto:greg@studio42dev.com" target="mailtogreg">
+                <button
+                  className="btn waves-effect waves-light grey darken-3 white-text text-darken-2"
+                  style={{ margin: "0 1em 1em 0" }}
+                >
+                  <i className="material-icons">mail</i>
+                </button>
+              </a>
               Contact me today to discuss a tailored solution for your business.
             </p>
           </div>
@@ -23,22 +25,38 @@ const Footer = () => {
             <h5 className="white-text">Links</h5>
             <ul>
               <li>
-                <a className="grey-text text-lighten-3" href="https://firebase.google.com/?authuser=0" target="blank">
+                <a
+                  className="grey-text text-lighten-3"
+                  href="https://firebase.google.com/?authuser=0"
+                  target="blank"
+                >
                   Firebase
                 </a>
               </li>
               <li>
-                <a className="grey-text text-lighten-3" href="https://reactjs.org/" target="blank">
+                <a
+                  className="grey-text text-lighten-3"
+                  href="https://reactjs.org/"
+                  target="blank"
+                >
                   React
                 </a>
               </li>
               <li>
-                <a className="grey-text text-lighten-3" href="https://materializecss.com/" target="blank">
+                <a
+                  className="grey-text text-lighten-3"
+                  href="https://materializecss.com/"
+                  target="blank"
+                >
                   MaterializeCSS
                 </a>
               </li>
               <li>
-                <a className="grey-text text-lighten-3" href="https://www.freecodecamp.org/" target="blank">
+                <a
+                  className="grey-text text-lighten-3"
+                  href="https://www.freecodecamp.org/"
+                  target="blank"
+                >
                   FreeCodeCamp
                 </a>
               </li>
@@ -50,14 +68,14 @@ const Footer = () => {
         <div className="container">
           © 2020 Studio42Dev
           <button
-          className="btn waves-effect waves-light grey darken-3 white-text text-darken-2 right"
-          onClick={(e) => {
-            e.preventDefault();
-            window.scrollTo({
-              top: 0,
-              behavior: "smooth"
-            });
-          }}
+            className="btn waves-effect waves-light grey darken-3 white-text text-darken-2 right"
+            onClick={(e) => {
+              e.preventDefault();
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              });
+            }}
           >
             <i className="material-icons left">arrow_upward</i>Back to top
           </button>
