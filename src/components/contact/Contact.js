151b35470b004be3cf7bc1aa8db8975054597d1d
Greg Brewton <gbrewton1980@gmail.com>
2020-05-16 22:13:43 -0600

    creates basic contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Contact = () => {
-    return ( <div id="contact">
-        <h1 className="center-align">Contact component</h1>
-    </div> );
-}
- 
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventdefault();
+  };
+
+  const [name, setName] = useState("Name");
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventdefault();
+        handleSubmit(e);
+      }}
+    >
+      <label>
+        Name: {name}
+        <input type="text" value={name} onChange={(e) => handleChange(e)} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+};
+
 export default Contact;
