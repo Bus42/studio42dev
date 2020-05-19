5eae1a2e3049b79dd5fd34e8f554b4f3cb76acb3
Greg Brewton <gbrewton1980@gmail.com>
2020-05-19 14:24:14 -0600

    - react-hook-form + Materialize.css form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,11 +1,15 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from "react";
 import "./contact.scss";
 
-export default function Contact() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+const handleSubmit = () => {
+  console.log("handleSubmit function called");
+};
 
+export default function Contact() {
+  const [firstName, setFirstName] = useState("First Name");
+  const [lastName, setLastName] = useState("Last Name");
+  const [reason, setReason] = useState("default");
+  const [email, setEmail] = useState("address@provider.com");
   return (
     <div id="contact">
       <div className="card">
@@ -16,25 +20,52 @@ export default function Contact() {
           <p>
             Fill out the form below and I will contact you as soon as possible.
           </p>
-          <div className="card-action">
-            <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="card-content">
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="firstName">What is your first name ?</label>
               <input
-                name="name"
-                ref={register({
-                  required: true
-                })}
-                placeholder="What is your name?"
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+                minLength="2"
+                type="text"
+                name="firstName"
+                placeholder={firstName}
               />
-              <h4>How can I help?</h4>
-              <select name="Reason for contact" ref={register}>
-                <option value="existing website">
-                  My website needs some work
-                </option>
-                <option value="new website">I need a website</option>
-                <option value="question">I'd like to ask a question</option>
-                <option value="comment">I have a comment</option>
-              </select>
-              <input type="submit" />
+              <label htmlFor="lastName">What is your last name ?</label>
+              <input
+                onChange={(e) => setLastName(e.target.value)}
+                required
+                minLength="2"
+                type="text"
+                name="lastName"
+                placeholder={lastName}
+              />
+              <label htmlFor="email">What email do I reply to?</label>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                name="email"
+                placeholder={email}
+              />
+              <div className="input-field col s12">
+                <select
+                  onChange={(e) => setReason(e.target.value)}
+                  required
+                  defaultValue={reason}
+                >
+                  <option value="default" disabled>
+                    Choose an option
+                  </option>
+                  <option value="existing website">
+                    My website needs some work
+                  </option>
+                  <option value="new website">I need a website</option>
+                  <option value="inquiry">I'd like to ask a question</option>
+                  <option value="feedback">I have a comment</option>
+                </select>
+                <label>How can I help?</label>
+              </div>
+              <input type="submit"/>
             </form>
           </div>
         </div>
