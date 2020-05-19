fb338a2c9f4ef2fd3ae9a9b7493662322f56cd81
Greg Brewton <gbrewton1980@gmail.com>
2020-05-19 14:56:22 -0600

    Changes state management

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import "./contact.scss";
 
-const handleSubmit = () => {
-  console.log("handleSubmit function called");
-};
-
 export default function Contact() {
-  const [firstName, setFirstName] = useState("First Name");
-  const [lastName, setLastName] = useState("Last Name");
-  const [reason, setReason] = useState("default");
-  const [email, setEmail] = useState("address@provider.com");
+  const [data, setData] = useState({
+    firstName: "First Name",
+    lastName: "Last Name",
+    email: "email@provider.com",
+    reason: "default",
+  });
+
+  const handleSubmit = () => {
+    console.log(data);
+  };
+
   return (
     <div id="contact">
       <div className="card">
@@ -22,36 +25,55 @@ export default function Contact() {
           </p>
           <div className="card-content">
             <form onSubmit={handleSubmit}>
-              <label htmlFor="firstName">What is your first name ?</label>
-              <input
-                onChange={(e) => setFirstName(e.target.value)}
-                required
-                minLength="2"
-                type="text"
-                name="firstName"
-                placeholder={firstName}
-              />
-              <label htmlFor="lastName">What is your last name ?</label>
-              <input
-                onChange={(e) => setLastName(e.target.value)}
-                required
-                minLength="2"
-                type="text"
-                name="lastName"
-                placeholder={lastName}
-              />
-              <label htmlFor="email">What email do I reply to?</label>
-              <input
-                onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                name="email"
-                placeholder={email}
-              />
-              <div className="input-field col s12">
-                <select
-                  onChange={(e) => setReason(e.target.value)}
+              <div className="input-field">
+                <input
+                  onChange={(e) => {
+                    let newData = { ...data };
+                    newData.firstName = e.target.value;
+                    setData(newData);
+                  }}
                   required
-                  defaultValue={reason}
+                  minLength="2"
+                  type="text"
+                  name="firstName"
+                  placeholder={data.firstName}
+                />
+                <input
+                  onChange={(e) => {
+                    let newData = { ...data };
+                    newData.lastName = e.target.value;
+                    setData(newData);
+                  }}
+                  required
+                  minLength="2"
+                  type="text"
+                  name="lastName"
+                  placeholder={data.lastName}
+                />
+                <label>What's your name?</label>
+              </div>
+              <div className="input-field">
+                <input
+                  onChange={(e) => {
+                    let newData = { ...data };
+                    newData.email = e.target.value;
+                    setData(newData);
+                  }}
+                  type="email"
+                  name="email"
+                  placeholder={data.email}
+                />
+                <label>Email</label>
+              </div>
+              <div className="input-field">
+                <select
+                  onChange={(e) => {
+                    let newData = { ...data };
+                    newData.reason = e.target.value;
+                    setData(newData);
+                  }}
+                  required
+                  defaultValue={data.reason}
                 >
                   <option value="default" disabled>
                     Choose an option
@@ -65,8 +87,13 @@ export default function Contact() {
                 </select>
                 <label>How can I help?</label>
               </div>
-              <input type="submit"/>
             </form>
+            <button
+              className="btn waves-effect waves-light grey darken-3 white-text text-darken-2 right"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
           </div>
         </div>
       </div>
