600b97bad8bb7749c41ac3105e6879782071e330
Greg Brewton <gbrewton1980@gmail.com>
2020-05-19 21:31:03 -0600

    Adds scroll listener to footer for collapsing

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./footer.scss";
 
 const Footer = () => {
+  const prevScrollY = useRef(0);
+  const [goingUp, setGoingUp] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (prevScrollY.current < currentScrollY && goingUp) {
+        setGoingUp(false);
+      }
+      if (prevScrollY.current > currentScrollY && !goingUp) {
+        setGoingUp(true);
+      }
+
+      prevScrollY.current = currentScrollY;
+      console.log(goingUp ? "up" : "down");
+    };
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [goingUp]);
+
   return (
     <footer className="page-footer grey darken-3">
       <div className="container">
