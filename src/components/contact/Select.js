d2bee9292aed67d51732b015d8e4f99007a78f04
Greg Brewton <gbrewton1980@gmail.com>
2020-05-20 19:38:12 -0600

    Adds dropdown with dynamic icons and text

---
+++ src/components/contact/Select.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "./select.scss";
+
+const Select = (props) => {
+  const [option, setOption] = useState("How can I help?");
+  const [icon, setIcon] = useState("arrow_drop_down");
+
+  const handleClick = (e, iconText) => {
+    const reason = e.target.text;
+    const newIcon = iconText;
+    console.log(reason);
+    setOption(reason);
+    setIcon(newIcon)
+    let newData = { ...props.data };
+    newData.reason = reason;
+    props.setData(newData);
+  };
+
+  return (
+    <div id="select">
+      <button
+        id="dropdown_trigger"
+        className="dropdown-trigger btn grey darken-3 white-text text-darken-2"
+        data-target="reason_dropdown"
+      >
+        <i className="material-icons left">{icon}</i>
+        {option}
+      </button>
+      <ul
+        id="reason_dropdown"
+        className="dropdown-content grey darken-3 white-text text-darken-2"
+      >
+        <li>
+          <i className="material-icons white-text left">code</i>
+          <a style={{display: "inline", color: "whitesmoke"}} onClick={(e) => handleClick(e, "code")} href="#!">Update Existing</a>
+        </li>
+        <li>
+          <i className="material-icons white-text left">devices</i>
+          <a style={{display: "inline", color: "whitesmoke"}} onClick={(e) => handleClick(e, "devices")} href="#!">New Website</a>
+        </li>
+        <li>
+          <i className="material-icons white-text left">help</i>
+          <a style={{display: "inline", color: "whitesmoke"}} onClick={(e) => handleClick(e, "help")} href="#!">Question</a>
+        </li>
+        <li>
+          <i className="material-icons white-text left">comment</i>
+          <a style={{display: "inline", color: "whitesmoke"}} onClick={(e) => handleClick(e, "comment")} href="#!">Feedback</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Select;
